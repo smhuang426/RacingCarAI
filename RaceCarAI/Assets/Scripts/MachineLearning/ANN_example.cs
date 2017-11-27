c7c875dbe21336fe98e650bfa9df6744ab3b6518
NoaSMHuangh <smhuang426@gmail.com>
2017-11-27 21:07:06 +0800

    Fix ANN example problem

--- RaceCarAI/Assets/Scripts/MachineLearning/ANN_example.cs
+++ RaceCarAI/Assets/Scripts/MachineLearning/ANN_example.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ANN_example : MonoBehaviour
-{
+public class ANN_example : MonoBehaviour {
 
 	// Use this for initialization
 	void Start ()
